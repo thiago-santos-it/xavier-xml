e9935886ddfa1bd6b361dfef1822cfd2aa070dd0
Thiago dos Santos <thiago.santos.it@gmail.com>
2024-04-08 15:20:24 -0300

    Removing DOM while not implemented

--- xavier_dom/src/dom/string.rs
+++
@@ -1,7 +0,0 @@
-/*
-WARNING: The original spec says that this type holds a UTF-16 STRING, since Rust works better with
-UTF-8 and the underling libs are working with UTF-8 these implementations uses the Rust standard.
-
-From: https://www.w3.org/TR/REC-DOM-Level-1/level-one-core.html
- */
-pub type DOMString = String;
