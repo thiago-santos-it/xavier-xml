69ac5606803ac020f67fac8607f070e6e2e09e88
Joel Köper <jk@joelkoeper.de>
2025-06-21 03:24:58 +0200

    Bump quick-xml version and properly re-export quick-xml crate to prevent the need of adding quick-xml as the enduser.

--- xavier_derive/src/deserialize/parser/enums.rs
+++ xavier_derive/src/deserialize/parser/enums.rs
@@ -10,16 +10,16 @@ impl XmlEnum {
             loop {
                 match reader.read_event() {
                     Err(error) =>  { return Err(PError::new(&format!("Error at position {}: {:?}", reader.buffer_position(), error))) },
-                    Ok(quick_xml::events::Event::Eof) => { },
-                    Ok(quick_xml::events::Event::Start(_)) => {},
-                    Ok(quick_xml::events::Event::End(_)) => {},
-                    Ok(quick_xml::events::Event::Empty(_)) => {},
-                    Ok(quick_xml::events::Event::Comment(_)) => {},
-                    Ok(quick_xml::events::Event::Text(event)) => { return Ok(String::from_utf8(event.to_vec())?.parse()?); },
-                    Ok(quick_xml::events::Event::CData(event)) => { return Ok(String::from_utf8(event.to_vec())?.parse()?); },
-                    Ok(quick_xml::events::Event::Decl(_)) => {},
-                    Ok(quick_xml::events::Event::PI(_)) => {},
-                    Ok(quick_xml::events::Event::DocType(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::Eof) => { },
+                    Ok(::xavier::quick_xml::events::Event::Start(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::End(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::Empty(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::Comment(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::Text(event)) => { return Ok(String::from_utf8(event.to_vec())?.parse()?); },
+                    Ok(::xavier::quick_xml::events::Event::CData(event)) => { return Ok(String::from_utf8(event.to_vec())?.parse()?); },
+                    Ok(::xavier::quick_xml::events::Event::Decl(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::PI(_)) => {},
+                    Ok(::xavier::quick_xml::events::Event::DocType(_)) => {},
                 }
             }
         }
